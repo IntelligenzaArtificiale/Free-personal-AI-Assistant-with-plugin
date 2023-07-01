9ea1f71ac2a1e72838dfebaf0827a6a25d156268
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-07-01 15:44:33 +0200

    Custom PlugIn prompt for chat
    This file contains all custom prompt used to build on top plugin

--- promptTemplate.py
+++ promptTemplate.py
@@ -1,3 +1,17 @@
+"""
+This file contains the template for the prompt to be used for injecting the context into the model.
+
+With this technique we can use different plugin for different type of question and answer.
+Like :
+- Internet
+- Data
+- Code
+- PDF
+- Audio
+- Video
+
+"""
+
 from datetime import datetime
 now = datetime.now()
 
@@ -39,7 +53,7 @@ def prompt4Code(prompt, context, solution):
     return final_prompt
 
 
-def prompt4PDF(prompt, context, solution):
+def prompt4Context(prompt, context, solution):
     final_prompt = f"""GENERAL INFORMATION : You is built by Alessandro Ciciarelli  the owener of intelligenzaartificialeitalia.net
                         ISTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE ,WRITE ALWAYS ONLY YOUR ACCURATE ANSWER!
                         PREVIUS MESSAGE : ({context})
@@ -66,3 +80,11 @@ def prompt4YT(prompt, context, solution):
                         THIS IS THE CORRECT ANSWER based on Youtube video gived in input : ({solution}) 
                         WITHOUT CHANGING ANYTHING OF CORRECT ANSWER , MAKE THE ANSWER MORE DETALIED:"""
     return final_prompt
+
+
+#HOW TO ADD YOUR OWN PROMPT :
+# 1) ADD YOUR FUNCTION HERE, for example : def prompt4Me(prompt, context):
+# 2) WRITE THE PROMPT TEMPLATE FOR YOUR FUNCTION, for example : template = f"YOU IS : {context} , NOW THE USER ASK : {prompt} . WRITE THE ANSWER :"
+# 3) RETURN THE TEMPLATE, for example : return template
+# 4) IMPORT YOUR FUNCTION IN THE MAIN FILE (streamlit_app.py) , for example : from promptTemplate import prompt4Me
+# 5) FOLLOW OTHER SPTEP IN THE MAIN FILE (streamlit_app.py)
