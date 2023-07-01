c3933b742a147788224de30b697be3d24156be7e
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-07-01 15:47:47 +0200

    Langchain Custom LLM Wrapper

--- HuggingChatAPI.py
+++ HuggingChatAPI.py
@@ -6,15 +6,18 @@ from typing import Optional, List, Mapping, Any
 from time import sleep
 
 
+# THIS IS A CUSTOM LLM WRAPPER Based on hugchat library
+# Reference :
+# - Langchain custom LLM wrapper : https://python.langchain.com/docs/modules/model_io/models/llms/how_to/custom_llm
+# - HugChat library : https://github.com/Soulter/hugging-chat-api
 
 class HuggingChat(LLM):
-    
-    history_data: Optional[List] = []
+    """HuggingChat LLM wrapper."""
     chatbot : Optional[hugchat.ChatBot] = None
     conversation : Optional[str] = ""
     email : Optional[str]
     psw : Optional[str]
-    #### WARNING : for each api call this library will create a new chat on chat.openai.com
+
     
     
     @property
@@ -24,11 +27,10 @@ class HuggingChat(LLM):
     def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
         if stop is not None:
             pass
-            #raise ValueError("stop kwargs are not permitted.")
-        #token is a must check
+
         if self.chatbot is None:
             if self.email is None and self.psw is None:
-                ValueError("Email and Password is required, pls check the documentation on github")
+                ValueError("Email and Password is required, pls check the documentation on github : https://github.com/Soulter/hugging-chat-api")
             else: 
                 if self.conversation == "":
                     sign = Login(self.email, self.psw) # type: ignore
@@ -39,19 +41,12 @@ class HuggingChat(LLM):
                 
                     id = self.chatbot.new_conversation()
                     self.chatbot.change_conversation(id)
-                    self.conversation = id
-                    
+                    self.conversation = id         
                 else:
-                    raise ValueError("Something went wrong")
+                    self.chatbot.change_conversation(self.conversation) # type: ignore
             
-        
-        sleep(2)
-        data = self.chatbot.chat(prompt, temperature=0.5, stream=False) # type: ignore
     
-        
-        #add to history
-        self.history_data.append({"prompt":prompt,"response":data})     # type: ignore
-        
+        data = self.chatbot.chat(prompt, temperature=0.4, stream=False) # type: ignore
         return data # type: ignore
 
     @property
@@ -61,7 +56,7 @@ class HuggingChat(LLM):
 
 
 
-#llm = HuggingChat(email = "YOUR-COOKIES-PATH" , psw ) #for start new chat
+#llm = HuggingChat(email = "YOUR-EMAIL" , psw = = "YOUR-PSW" ) #for start new chat
 
 
 #print(llm("Hello, how are you?"))
